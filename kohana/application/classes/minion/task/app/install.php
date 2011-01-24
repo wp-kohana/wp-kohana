e541e0c056517f631a5ec27cc36e03a2cad11391
Lorenzo Pisani <zeelot3k@gmail.com>
2011-01-23 22:35:13 -0700

    refactoring to allow reusing of data for other purposes

--- kohana/application/classes/minion/task/app/install.php
+++ kohana/application/classes/minion/task/app/install.php
@@ -20,49 +20,49 @@ class Minion_Task_App_Install extends Minion_Task {
 	 */
 	public function execute(array $config)
 	{
+		// Generate kohana/application/bootstrap.php
 		fwrite(STDOUT, "Bootstrap Configuration\n");
-		$this->setup_bootstrap();
 
-		fwrite(STDOUT, "Database Configuration\n");
-		$this->setup_db_config();
-
-		return;
-	}
-
-	protected function setup_bootstrap()
-	{
+		$data = $this->get_bootstrap_values();
 		$view = Kostache::factory('minion/bootstrap')
-			->bind('timezone', $timezone)
-			->bind('locale', $locale)
-			->bind('language', $language)
-			->bind('base_url', $base_url);
-
-		$timezone = $this->ask('Timezone', 'America/Chicago');
-		$locale = $this->ask('Locale', 'en_US.utf-8');
-		$language = $this->ask('Language', 'en-us');
-		$base_url = $this->ask('Base URL', 'http://dev.vm/wp-kohana/');
+			->set($data);
 
 		$filepath = APPPATH.'bootstrap.php';
 		file_put_contents($filepath, $view->render());
 		fwrite(STDOUT, "File written to ".$filepath."\n");
-	}
 
-	protected function setup_db_config()
-	{
+		// Generate kohana/application/config/database.php
+		fwrite(STDOUT, "Database Configuration\n");
+
+		$data = $this->get_database_values();
 		$view = Kostache::factory('minion/config/database')
-			->bind('hostname', $hostname)
-			->bind('database', $database)
-			->bind('username', $username)
-			->bind('password', $password);
-
-		$hostname = $this->ask('Hostname', 'localhost');
-		$database = $this->ask('Database');
-		$username = $this->ask('Username');
-		$password = $this->ask('Password');
+			set($data);
 
 		$filepath = APPPATH.'config/database.php';
 		file_put_contents($filepath, $view->render());
 		fwrite(STDOUT, "File written to ".$filepath."\n");
+
+		return;
+	}
+
+	protected function get_bootstrap_values()
+	{
+		return array(
+			'timezone' => $this->ask('Timezone', 'America/Chicago'),
+			'locale'   => $this->ask('Locale', 'en_US.utf-8'),
+			'language' => $this->ask('Language', 'en-us'),
+			'base_url' => $this->ask('Base URL', 'http://dev.vm/wp-kohana/'),
+		);
+	}
+
+	protected function get_database_values()
+	{
+		return array(
+			'hostname' => $this->ask('Hostname', 'localhost'),
+			'database' => $this->ask('Database'),
+			'username' => $this->ask('Username'),
+			'password' => $this->ask('Password'),
+		);
 	}
 
 	protected function ask($message, $default = NULL)
